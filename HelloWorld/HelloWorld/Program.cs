bcf84ffb26f7091e15fb9a292f4e69be2035aef5
huhuapop <huhuapop@outlook.com>
2018-10-27 19:35:36 -0400

    hello world

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+
+            }
+           
+        }
+    }
+}
